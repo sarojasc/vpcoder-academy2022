8e750777db00fb8939d7954428867b950f83c274
sarojasc <saruchikkegoud@gmail.com>
2022-06-10 23:28:10 +0530

    arrays commit

---
+++ src/com/ArrayPrntRvrc.java
@@ -0,0 +1,14 @@
+package com;
+
+public class ArrayPrntRvrc {
+    public static void main(String[] args) {
+        int[] arrays={1,2,3,4,5};
+        int B=0;
+        int S=0;
+        for(int i= arrays.length-1;i>=0;i--)
+        {
+            System.out.println( arrays[i]);
+        }
+
+    }
+}
